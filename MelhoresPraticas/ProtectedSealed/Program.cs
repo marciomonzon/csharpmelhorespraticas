5a3156e8792764b595955a15830c9b08e4a1b276
Marcio <marcio.pcmonzon@gmail.com>
2020-06-01 20:44:45 -0300

    parte 4

---
+++ MelhoresPraticas/ProtectedSealed/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace ProtectedSealed
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+
+        class Person
+        {
+            public string FirstName { get; set; }
+            public string LastName { get; set; }
+        }
+
+        sealed class Employee : Person
+        {
+            // não tem necessidade de ser protected
+            // a classe já é sealed
+            //protected string EmployeeCode { get; set; }
+
+            private string EmployeeCode { get; set; }
+        }
+
+        // não é possível herdar classe sealed
+        //class Accountant : Employee
+        //{
+
+        //}
+    }
+}
---
+++ MelhoresPraticas/Variaveis/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace Variaveis
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+
+        public int GetValue()
+        {
+            //int n1 = int.MinValue;
+            //int n2 = int.MinValue;
+            int n1;
+            int n2;
+
+            n1 = GetNumber1();
+            n2 = GetNumber2();
+
+            return n1 + n2;
+        }
+
+        public int GetNumber1()
+        {
+            return 1;
+        }
+
+        public int GetNumber2()
+        {
+            return 2;
+        }
+    }
+}
