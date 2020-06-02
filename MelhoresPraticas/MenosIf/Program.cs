f47b025f06361a434f8fec9b5a03e89dc5da66d2
Marcio <marcio.pcmonzon@gmail.com>
2020-06-01 21:23:47 -0300

    if resumido

---
+++ MelhoresPraticas/MenosIf/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace MenosIf
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            int n1 = 4;
+            int n2 = 5;
+
+            // ruim
+            //if (n1 > 5)
+            //{
+            //    if (n2 > 5)
+            //    {
+
+            //    }
+            //}
+
+            if ((n1 > 5) && (n2 > 5))
+            {
+
+            }
+
+        }
+    }
+}
